754243dac67d8b9cdb72720df0028edfd1f24543
chuahce <choongeng.chuah@gmail.com>
2023-02-27 17:39:18 +0800

    Initial Commit

---
+++ src/main/java/nus/iss/tfip/ssf/day13_ssflecture/Day13SsflectureApplication.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.ssf.day13_ssflecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day13SsflectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day13SsflectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/tfip/ssf/day13_ssflecture/Day13SsflectureApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.ssf.day13_ssflecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day13SsflectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
